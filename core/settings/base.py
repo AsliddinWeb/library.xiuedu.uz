90fa0818c645de192b53c685e0261dd1dbb65191
AsliddinWeb <asliddinsinger@gmail.com>
2025-01-07 16:26:31 +0500

    Added static root

--- core/settings/base.py
+++ core/settings/base.py
@@ -72,7 +72,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
