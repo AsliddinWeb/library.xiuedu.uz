70fd121ccd6a7af157fde3c9dc088912f4027f51
AsliddinWeb <asliddinsinger@gmail.com>
2025-01-07 16:39:10 +0500

    Edited settings

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,6 +1,14 @@
 import os
 
+from dotenv import load_dotenv
+
+# loading env
+load_dotenv()
+
 environment = os.getenv('DJANGO_ENV', 'local')
+
+print(environment)
+print("+")
 
 if environment == 'production':
     from .production import *
